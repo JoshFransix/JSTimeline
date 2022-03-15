a98c6f6eb0235e1c34e665885a79dc29ae02ccfa
joshfransix <joshiandersonk69@gmail.com>
2022-03-15 21:17:32 +0100

    js

---
+++ scrollAnim.js
@@ -0,0 +1,17 @@
+const line = document.querySelector(".timeline-innerline");
+const timeline_events = document.querySelectorAll("ul li");
+
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // show timeline event
+      timeline_events.forEach((e) => {
+        showTime(e);
+        line.style.width = "100%";
+      });
+    }
+  });
+});
+
+let target = document.querySelector(".timeline ul");
+observer.observe(target);
