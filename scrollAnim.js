76b0ebdd34f38cb76917ea0a46d6a58b8b85a16e
joshfransix <joshiandersonk69@gmail.com>
2022-03-15 22:11:10 +0100

    responsiveStyling

--- scrollAnim.js
+++ scrollAnim.js
@@ -1,14 +1,25 @@
 const line = document.querySelector(".timeline-innerline");
 const timeline_events = document.querySelectorAll("ul li");
 
+let i = 0;
+
+function slowLoop() {
+  setTimeout(function () {
+    showTime(timeline_events[i]);
+    line.style.width = `${((i + 1) / timeline_events.length) * 100}%`;
+    i++;
+
+    if (i < timeline_events.length) {
+      slowLoop();
+    }
+  }, 800);
+}
+
 let observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       // show timeline event
-      timeline_events.forEach((e) => {
-        showTime(e);
-      });
-      line.style.width = "100%";
+      slowLoop();
     }
   });
 });
--- ui.js
+++ ui.js
@@ -3,4 +3,35 @@ function showTime(e) {
   e.querySelector(".timeline-point").style.background = "blue";
   e.querySelector(".date").style.opacity = "100%";
   e.querySelector("p").style.opacity = "100%";
+  e.querySelector("p").style.transform = "translateY(0px)";
 }
+
+function hideTime(e) {
+  e.removeAttribute("done");
+  e.querySelector(".timeline-point").style.background = `rgb(228,228,228)`;
+  e.querySelector(".date").style.opacity = "0%";
+  e.querySelector("p").style.opacity = "0%";
+  e.querySelector("p").style.transform = `translateY(-10px)`;
+}
+
+timeline_events.forEach((li, index) => {
+  li.addEventListener("click", () => {
+    if (li.getAttribute("done")) {
+      line.style.width = `${(index / timeline_events.length) * 100}%`;
+      // hide all timeline events from last up until point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx >= index) {
+          hideTime(ev);
+        }
+      });
+    } else {
+      line.style.width = `${(index + 1) / timeline_events.length}%`;
+      // show all timeline events from first up until point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx <= index) {
+          showTime(ev);
+        }
+      });
+    }
+  });
+});
